3f76b5d321f4a7f88e418bfe220154968de13b70
Maxula0234 <max0234@icloud.com>
2020-06-06 20:45:13 +0300

    update

--- src/main/java/org/levelup/chat/jdbc/BaseClass.java
+++ src/main/java/org/levelup/chat/jdbc/BaseClass.java
@@ -1,9 +1,5 @@
 package org.levelup.chat.jdbc;
 
-import org.levelup.chat.UserService;
-
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.logging.Logger;
 
 public class BaseClass {
